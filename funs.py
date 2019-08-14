54568ae724d1f13a77c339cee7ad5bba9ca6d97b
Thomas <thomasdigiusto@me.com>
2019-08-14 07:59:36 +0100

    cracked into several files

---
+++ funs.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+import time 
+from datetime import datetime
+
+def get_price(URL):
+	headers = {
+		"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
+
+	print("[+] Contacting the page")
+	page = requests.get(URL, headers=headers)
+	print("[+] Page received")
+
+	soup1 = BeautifulSoup(page.content, 'lxml')
+
+	soup2 = BeautifulSoup(soup1.prettify(), 'html.parser')
+
+	price = soup1.find(id="priceblock_ourprice").get_text()[1:]
+	f_price = float(price)
+	print("[+] Price found: %s" % f_price)
+
+	return f_price
